fa1f9fc841dfab01b9d0087434623fcbdb1399c9
Jalilov <javohirjalilov777@gmail.com>
2022-10-22 14:50:17 +0500

    keyboard button

---
+++ keyboard.py
@@ -0,0 +1,25 @@
+import requests
+
+TOKEN = '5559122728:AAHOu1gL4pA1riJPMCmICNTKI57P5xnHsyA'
+chat_id = 5575549228
+
+def send_message(chat_id,text,TOKEN):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+    button1 = {'text':'CAT'}
+    button2 = {'text':'DOG'}
+
+    keyboard = [[button1,button2]]
+
+    reply_markup = {'keyboard':keyboard}
+    
+    data = {
+        'chat_id':chat_id,
+        'text':text,
+        'reply_markup':reply_markup
+    }
+    r = requests.post(url,json=data)
+    print(r.status_code)
+
+
+send_message(chat_id, 'Hi', TOKEN)
